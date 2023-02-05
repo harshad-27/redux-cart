6099ca5e952aa60f1f51d133af55520583ee4396
harshad-27 <harshadshinde118@gmail.com>
2023-02-05 13:33:50 +0530

    remove basepath

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
               </Box>
              <Box className="z-10">
              <Routes>
-                <Route path="/" element={<Home />} />
+                <Route exact path="/" element={<Home />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/products" element={<Products />} />
               </Routes>
